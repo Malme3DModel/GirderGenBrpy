e25e66a4b2442f99102552afaeadfb6373ee4aa7
sasaco <sasaicco@gmail.com>
2022-08-09 21:20:48 +0900

    任意形状を描いてみた

--- tests/test_ifc.py
+++ tests/test_ifc.py
@@ -1,15 +1,19 @@
-import ifcopenshell
-from ifcopenshell import geom
-
-# 自作モジュール
-from src.ifc_01 import IfcManager
-
-def test_ifcopenshell():
+# H鋼の生成テスト
+from src.ifc_H import IfcManager
+def test_ifc_H():
     ifc = IfcManager()
-    ifc.add_Beam()
+    ifc.add_Beam(W=0.2 ,D=0.3 , tw=0.012 , tf=0.012  , r = 2*0.012,
+                    L=4.00 ,position=(0.0,0.0,0.0) , direction=(1.0,0.0,0.0))
 
+# スラブの生成テスト
+from src.ifc_slab import IfcManager
+def test_ifc_slab():
+    ifc = IfcManager()
+    ifc.add_Slab(b=3 ,h=0.3,
+                    L=4.00 ,position=(0.0,0.0,0.0) , direction=(1.0,0.0,0.0))
 
 
 if __name__ == "__main__":
 
-    test_ifcopenshell()
+    test_ifc_slab()
+
