91c3a467548bb0c3be979fadf30e25248e9f69fd
sasaco <sasaicco@gmail.com>
2022-08-20 22:40:34 +0900

    sf

--- tests/test_ifc.py
+++ tests/test_ifc.py
@@ -3,6 +3,29 @@ from src.Girder import Girder
 # H鋼の生成テスト
 def test_Beam():
     girder = Girder()
+    girder.add_Hsteel(L=10.0,D=0.3,W=0.2,tf=0.012,tw=0.012,T=1.0,amount=3.0,interval=1.0)
+
+    return girder.ifc.file
+
+
+# スラブの生成テスト
+def test_Slab():
+    girder = Girder()
+    girder.add_Slab(L=10.0, B=5.0, b=0.5, H=1.0, T=0.5, i=0.02)
+
+    return girder.ifc.file
+
+
+# 鉄筋の生成テスト
+def test_Rebar():
+    girder = Girder()
+    girder.add_Rebar(position=(0.0,0.0,0.0) , direction=(1.0,0.0,0.0))
+
+    return girder.ifc.file
+
+# H鋼の生成テスト
+def test_Beam():
+    girder = Girder()
     girder.add_Beam(L=10.0,D=0.3,W=0.2,tf=0.012,tw=0.012,T=1.0,amount=3.0,interval=1.0)
 
     return girder.ifc.file
