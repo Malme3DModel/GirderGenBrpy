6f77cf5a90ba3656da8430ae8f7aab3607cc250a
yuluji <yuji.hirano@malme.co.jp>
2022-10-06 10:45:04 +0900

    部材ごとに属性情報を付与（主桁のみ）

--- src/ifcGirder.py
+++ src/ifcGirder.py
@@ -50,7 +50,6 @@ def createIfcGirder(body):
     endbeam = body['endbeam']
     v_endbeam ,f_endbeam = createObject(endbeam)
     crossbeam = body['crossbeam']
-    v_crossbeam ,f_crossbeam = createObject(crossbeam)
 
     # obj ファイルを ifc に変換
     # ifcファイルを生成
@@ -58,44 +57,15 @@ def createIfcGirder(body):
     ifc = ifcProject(ProjectName, "橋梁")
     # モデル空間を作成
     # 階層2のオブジェクト名を指定
-    Container = ifc.create_place("上部構造")
+    Container = ifc.create_place(Name="上部構造", ID="1", Class='上部構造', Info='', Type="単径間鋼橋鈑桁")
     Obj = ifcObj(ifc)
         # 主桁を格納する場所を追加
-    import ifcopenshell
-    B0 = ifc.file.create_entity(
-        "IfcBuildingElementProxy",
-        GlobalId = create_guid(),
-        Name = "主桁を格納する場所"
-    )
-    B0_Placement = ifc.file.createIfcLocalPlacement(Container.ObjectPlacement)
-    B0.ObjectPlacement=B0_Placement
-
-    Flr1_Container = ifc.file.createIfcRelContainedInSpatialStructure(create_guid(), ifc.owner_hist)
-    Flr1_Container.RelatedElements=[B0]
-    Flr1_Container.RelatingStructure= Container
-
+    beamBox = Obj.CreateBox(Container=Container, Name='主桁',ID="1", Class='主桁', Info='', Type="鋼橋鈑桁")
     #モデルの追加
     # 階層3のオブジェクト名を指定
     for i in range(len(v_beam)):
-        Info = '主桁{:0=2}'.format(i+1)
-        Obj.add_Obj(vertices=v_beam[i], faces=f_beam[i], Container=Container, Name3="主桁", ID=str(i), Class='主桁', Info=Info, Type='鋼橋鈑桁')
-
-    ID_mid = 0
-    for i in range(len(v_mid_u)):
-        Info_l = '下横構{:0=2}'.format(i+1)
-        Info_u = '上横構{:0=2}'.format(i+1)
-        Obj.add_Obj(vertices=v_mid_l[i], faces=f_mid_l[i], Container=Container, Name3="横構", ID=str(ID_mid), Class='下横構', Info=Info_l, Type='')
-        Obj.add_Obj(vertices=v_mid_u[i], faces=f_mid_u[i], Container=Container, Name3="横構", ID=str(ID_mid+1), Class='上横構', Info=Info_u, Type='')
-        ID_mid += 2
-
-    for i in range(len(v_endbeam)):
-        Info = '端横桁{:0=2}'.format(i+1)
-        Obj.add_Obj(vertices=v_endbeam[i], faces=f_endbeam[i], Container=Container, Name3="端横桁", ID=str(i), Class='端横桁', Info=Info, Type='')
-
-    for i in range(len(v_endbeam)):
-        Info = '端横桁{:0=2}'.format(i+1)
-        Obj.add_Obj(vertices=v_endbeam[i], faces=f_endbeam[i], Container=Container, Name3="端横桁", ID=str(i), Class='端横桁', Info=Info, Type='')
-
+        Name = '主桁{:0=2}'.format(i+1)
+        Obj.add_Obj(vertices=v_beam[i], faces=f_beam[i], Container=beamBox, Name3=Name, ID=str(i+1), Class=Name, Info='', Type='鋼橋鈑桁')
 
     ifcFile = ifc.file
     # ifc ファイルをテキストに変換する
